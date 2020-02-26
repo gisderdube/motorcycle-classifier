7389b1c80148acfab8e9e4856d35f391b787a49c
Lukas Gisder-Dubé <gisderdube@gmail.com>
2020-02-26 18:31:07 +0100

    remove public dir

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,11 @@
 npm run build
-curl https://dube-hacking.s3.eu-central-1.amazonaws.com/export.pkl --output server/cnn/export.pkl
-curl https://dube-hacking.s3.eu-central-1.amazonaws.com/stage-final.pth --output server/cnn/models/stage-final.pth
+
+pip install fastai
+
+if [ ! -f server/cnn/export.pkl ]; then
+    curl https://dube-hacking.s3.eu-central-1.amazonaws.com/export.pkl --output server/cnn/export.pkl
+fi
+
+if [ ! -f server/cnn/models/stage-final.pth ]; then
+    curl https://dube-hacking.s3.eu-central-1.amazonaws.com/stage-final.pth --output server/cnn/models/stage-final.pth
+fi
